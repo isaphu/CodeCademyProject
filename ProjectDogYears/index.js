0814d005a6c7100c9dd9b42b427ed0eeb301058f
uronohan <uronohan@gmail.com>
2020-02-23 20:19:42 +0700

    This is Project Dog years on codecademy for JavaScript practice

---
+++ ProjectDogYears/index.js
@@ -0,0 +1,19 @@
+let myAge = 22;
+// this is my age 
+let earlyYears = 2;
+//this vairable will be change
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+//this will change 
+laterYears *= 4
+
+// console.log(earlyYears);
+// console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears
+
+myName = 'Isa'
+myName.toLowerCase('Isa');
+
+console.log(`My name isa ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+//I interpolate this sentence using variable myName myAge and myAgeInDogYears. 
